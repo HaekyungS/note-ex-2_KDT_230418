a0043a72cbe3fe8502d9377b533f9a0ce2fade03
HaekyungSeong <haekyungs95@gmail.com>
2023-04-18 20:38:00 +0900

    function exampleOne clear

--- 성해경.js
+++ 성해경.js
@@ -7,9 +7,19 @@ let basicData = {
 
 function exampleOne(count, min, max){
   let exampleArr = []
-
+  for(let i=0; i<count; i++){
+    exampleArr.push(parseFloat((Math.random()*(max-min)+min).toFixed(2)))
+    // 새로 알게된 메서드
+    // parseFloat() : 실수(float)로 변환
+    // toFixed() : 소수점 자릿수 지정
+  }
+  // console.log(exampleArr)
+  // 콘솔로 확인
   return exampleArr
 }
+
+// console.log(exampleOne(basicData.count, basicData.min, basicData.max))
+// 콘솔로 작동 확인
 
 function exampleTwo(array, dixideNumber){
   let exampleObject = {
