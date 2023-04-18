db3ee5ecd798ddf2d4f879c533d3311e1ef0ddaa
HaekyungSeong <haekyungs95@gmail.com>
2023-04-18 20:16:30 +0900

    example 기본

---
+++ 성해경.js
@@ -0,0 +1,38 @@
+let basicData = {
+  count: 10,
+  min : 1,
+  max : 10,
+  dixideNumber : 5
+}
+
+function exampleOne(count, min, max){
+  let exampleArr = []
+
+  return exampleArr
+}
+
+function exampleTwo(array, dixideNumber){
+  let exampleObject = {
+    a:[],
+    b:[]
+  }
+
+  return exampleObject
+}
+
+function exampleThree(array){
+  let value;
+  return value
+}
+
+function exampleFour(object){
+  let value;
+  return value
+}
+
+function exampleFive(objectOne, objectTwo){
+  let value;
+  return value
+}
+
+console.log(exampleFive(basicData, exampleTwo()))
